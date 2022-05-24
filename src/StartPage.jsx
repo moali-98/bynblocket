58ed59ebbd26b8719b0ff79bea3ee207b029e02f
moali-98 <moali_98@hotmail.com>
2022-05-24 23:44:26 +0200

    first commit

---
+++ src/StartPage.jsx
@@ -0,0 +1,7 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+export default function StartPage() {
+  return <Container><Row><Col>
+    <h1>Välkommen!</h1>
+  </Col></Row></Container>
+}
