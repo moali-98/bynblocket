c80cf2dadff1b993cb7c5b3acae5a3066dffb9be
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-05-26 19:27:06 +0200

    search

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,34 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Card, Input } from 'semantic-ui-react'
-export default function Posts() {
-    const [APIData, setAPIData] = useState([])
-    useEffect(() => {
-        axios.get(`http://localhost:3000/api/products`)
-            .then((response) => {
-                setAPIData(response.data);
-            })
-    }, [])
-
+import React, { Component } from 'react'
+import axios from 'axios'
+class LiveSearchFilter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      Profile: [],
+    }
+    this.cancelToken = ''
+    this.getVal = this.getVal.bind(this)
+    this.node = React.createRef()
+  }
+  componentDidMount() {
+    document.addEventListener('mousedown', this.getVal)
+  }
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.getVal)
+  }
+  getVal = (e) => {
+    if (this.node.current.contains(e.target)) {
+      return
+    }
+    this.setState({
+      userList: [],
+    })
+  }
+  onChange = async (e) => {
+    if (this.cancelToken) {
+      this.cancelToken.cancel()
+    }
+    this.cancelToken = axios.CancelToken.source()
+    await axios
+      .get('http://localhost:3000/api/products', {
+        cancelToken: this.cancelToken.token,
+      })
+      .then((res) => {
+        this.setState({
+          Profile: res.data,
+        })
+      })
+      .catch((e) => {
+        if (axios.isCancel(e) || e) {
+          console.log('Data not found.')
+        }
+      })
+    let stringKwd = e.target.value.toLowerCase()
+    let filterData = this.state.Profile.filter((item) => {
+      return item.username.toLowerCase().indexOf(stringKwd) !== -1
+    })
+    this.setState({
+      Profile: filterData,
+    })
+  }
+  render() {
     return (
-        <div style={{ padding: 20 }}>
-            <Input icon='search'
-                placeholder='Search...'
-            />
-            <Card.Group itemsPerRow={3} style={{ marginTop: 20 }}>
-                {APIData.map((product) => {
-                    return (
-                        <Card>
-                            <Card.Content>
-                                <Card.Header>{product.name}</Card.Header>
-
-
-
-                            </Card.Content>
-                        </Card>
-                    )
-                })}
-            </Card.Group>
+      <>
+        <h3>Search</h3>
+        <div className="input-group mt-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Find ..."
+            ref={this.node}
+            onClick={this.getVal}
+            onChange={this.onChange}
+          />
         </div>
+        <div className="list-group">
+          {this.state.Profile.map((products) => {
+            return (
+              <a
+                className="list-group-item list-group-item-action"
+                key={products.id}
+              >
+                {products.name}
+              </a>
+            )
+          })}
+        </div>
+      </>
     )
+  }
 }
+export default LiveSearchFilter;
